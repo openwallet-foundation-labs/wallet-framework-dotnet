832adb0ded0617fa6ac7515af790d1cee421517f
Kevin Dinh <92726228+Dindexx@users.noreply.github.com>
2024-01-29 13:14:12 +0100

    fix oidvci credential endpoint (#35)
    Signed-off-by: Kevin <kevin.dinh@lissi.id>

--- src/Hyperledger.Aries/Features/OpenID4VC/VCI/Services/Oid4VciClientService/IOid4VciClientService.cs
+++ src/Hyperledger.Aries/Features/OpenID4VC/VCI/Services/Oid4VciClientService/IOid4VciClientService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Hyperledger.Aries.Features.OpenId4Vc.Vci.Models.Authorization;
 using Hyperledger.Aries.Features.OpenId4Vc.Vci.Models.CredentialResponse;
+using Hyperledger.Aries.Features.OpenId4Vc.Vci.Models.Metadata.Credential;
 using Hyperledger.Aries.Features.OpenId4Vc.Vci.Models.Metadata.Issuer;
 
 namespace Hyperledger.Aries.Features.OpenId4Vc.Vci.Services.Oid4VciClientService
@@ -23,18 +24,17 @@ namespace Hyperledger.Aries.Features.OpenId4Vc.Vci.Services.Oid4VciClientService
         /// <summary>
         ///     Requests a verifiable credential using the provided parameters.
         /// </summary>
-        /// <param name="credentialIssuer">The credential issuer's identifier.</param>
-        /// <param name="clientNonce">A nonce value for the client.</param>
-        /// <param name="type">The type of the credential being requested.</param>
+        /// <param name="credentialMetadata">The credential metadata.</param>
+        /// <param name="issuerMetadata">The issuer metadata.</param>
         /// <param name="tokenResponse">The token response from the previous token request.</param>
         /// <returns>
         ///     A tuple containing the credential response and the key ID used during the signing of the Proof of Possession.
         /// </returns>
         Task<(OidCredentialResponse, string)> RequestCredentialAsync(
-            string credentialIssuer,
-            string clientNonce,
-            string type,
-            TokenResponse tokenResponse);
+            OidCredentialMetadata credentialMetadata,
+            OidIssuerMetadata issuerMetadata,
+            TokenResponse tokenResponse
+        );
 
         /// <summary>
         ///     Requests a token using the provided issuer metadata and pre-authorized code.
@@ -43,6 +43,10 @@ namespace Hyperledger.Aries.Features.OpenId4Vc.Vci.Services.Oid4VciClientService
         /// <param name="preAuthorizedCode">The pre-authorized code for token request.</param>
         /// <param name="transactionCode">The Transaction Code.</param>
         /// <returns>A task that represents the asynchronous operation. The task result contains the token response.</returns>
-        Task<TokenResponse> RequestTokenAsync(OidIssuerMetadata metadata, string preAuthorizedCode, string? transactionCode = null);
+        Task<TokenResponse> RequestTokenAsync(
+            OidIssuerMetadata metadata,
+            string preAuthorizedCode,
+            string? transactionCode = null
+        );
     }
 }
