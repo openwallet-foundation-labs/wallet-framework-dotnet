2e5a199efd2ed99945581ad2884db9f79186c878
Sebastian Bickerle <sebastian.bickerle@lissi.id>
2024-06-09 20:46:57 +0200

    added test for pex service with nested credential
    Signed-off-by: Sebastian Bickerle <sebastian.bickerle@lissi.id>

--- test/WalletFramework.Oid4Vc.Tests/PresentationExchange/Services/CredentialExamples.cs
+++ test/WalletFramework.Oid4Vc.Tests/PresentationExchange/Services/CredentialExamples.cs
@@ -21,4 +21,34 @@ public static class CredentialExamples
         { "degree", "Master of Science" },
         { "universityName", "ExampleUniversity" }
     };
+    
+    public static JObject NestedCredential => new()
+    {
+        {"vct", "IdentityCredential"},
+        {"iss", "did:example:issuer"},
+        new SdProperty("first_name", "John"),
+        new SdProperty("last_name", "Doe"),
+        new SdProperty("address", new JObject
+        {
+            new SdProperty("street", "21 2nd Street"),
+            new SdProperty("city", "New York"),
+            new SdProperty("state", "NY"),
+            new SdProperty("postal_code", "10021")
+        })
+    };
+    
+    public static JObject AlternativeNestedCredential => new()
+    {
+        {"vct", "IdentityCredential"},
+        {"iss", "did:example:issuer"},
+        new SdProperty("first_name", "Erika"),
+        new SdProperty("last_name", "Mustermann"),
+        new SdProperty("address", new JObject
+        {
+            new SdProperty("street", "Schuldstr. 15"),
+            new SdProperty("city", "Berlin"),
+            new SdProperty("state", "BE"),
+            new SdProperty("postal_code", "12345")
+        })
+    };
 }
