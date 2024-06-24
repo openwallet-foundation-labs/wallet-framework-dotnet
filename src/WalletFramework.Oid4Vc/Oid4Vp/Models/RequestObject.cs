28e63c5a368c4dbdaf7202246b5e4cfef9d79ed9
Johannes Tuerk <johannes.tuerk@lissi.id>
2024-06-24 16:27:44 +0200

    add wildcard capabilities
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

--- src/WalletFramework.Oid4Vc/Oid4Vp/Models/RequestObject.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vp/Models/RequestObject.cs
@@ -8,139 +8,138 @@ using static WalletFramework.Oid4Vc.Oid4Vp.Models.AuthorizationRequest;
 using static Newtonsoft.Json.Linq.JArray;
 using static System.Convert;
 
-namespace WalletFramework.Oid4Vc.Oid4Vp.Models
+namespace WalletFramework.Oid4Vc.Oid4Vp.Models;
+
+/// <summary>
+///     Represents a request object as defined in the context of OpenID4VP.
+/// </summary>
+public readonly struct RequestObject
+{
+    private JwtSecurityToken Value { get; }
+
+    /// <summary>
+    ///     The client ID scheme used to obtain and validate metadata of the verifier.
+    /// </summary>
+    public ClientIdScheme ClientIdScheme => AuthorizationRequest.ClientIdScheme;
+
+    /// <summary>
+    ///     The client ID of the verifier.
+    /// </summary>
+    public string ClientId => AuthorizationRequest.ClientId;
+
+    private AuthorizationRequest AuthorizationRequest { get; }
+
+    /// <summary>
+    ///     Creates a new instance of the <see cref="RequestObject" /> class.
+    /// </summary>
+    public static implicit operator RequestObject(JwtSecurityToken token) => new(token);
+
+    /// <summary>
+    ///     Creates a new instance of the <see cref="RequestObject" /> class.
+    /// </summary>
+    public static implicit operator JwtSecurityToken(RequestObject requestObject) => requestObject.Value;
+
+    private RequestObject(JwtSecurityToken token)
+    {
+        AuthorizationRequest = CreateAuthorizationRequest(token.Payload.SerializeToJson());
+        Value = token;
+    }
+
+    /// <summary>
+    ///     Creates a new instance of the <see cref="RequestObject" /> class.
+    /// </summary>
+    public static RequestObject CreateRequestObject(string requestObjectJson)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var jwt = tokenHandler.ReadJwtToken(requestObjectJson);
+
+        return new RequestObject(jwt);
+    }
+
+    /// <summary>
+    ///     Gets the authorization request from the request object.
+    /// </summary>
+    public AuthorizationRequest ToAuthorizationRequest() => AuthorizationRequest;
+}
+
+/// <summary>
+///     Extension methods for <see cref="RequestObject" />.
+/// </summary>
+public static class RequestObjectExtensions
 {
     /// <summary>
-    ///     Represents a request object as defined in the context of OpenID4VP.
+    ///     Validates the JWT signature.
     /// </summary>
-    public readonly struct RequestObject
+    /// <returns>The validated request object.</returns>
+    /// <exception cref="InvalidOperationException">Throws when validation fails</exception>
+    public static RequestObject ValidateJwt(this RequestObject requestObject)
+        => ((JwtSecurityToken)requestObject).IsSignatureValid(requestObject.GetLeafCertificate().GetPublicKey())
+            ? requestObject
+            : throw new InvalidOperationException("Invalid JWT Signature");
+
+    /// <summary>
+    ///     Validates the SAN name of the leaf certificate
+    /// </summary>
+    /// <returns>The validated request object</returns>
+    /// <exception cref="InvalidOperationException">Throws when validation fails</exception>
+    public static RequestObject ValidateSanName(this RequestObject requestObject)
     {
-        private JwtSecurityToken Value { get; }
+        var x509Certificate = new X509Certificate2(
+            requestObject.GetLeafCertificate().GetEncoded()
+        );
+        
+        return GetSanDnsNames(x509Certificate).Any(sanDnsName => requestObject.ClientId.EndsWith(sanDnsName.Split("*").Last()))
+            ? requestObject
+            : throw new InvalidOperationException("SAN does not match Client ID");
+    }
 
-        /// <summary>
-        ///     The client ID scheme used to obtain and validate metadata of the verifier.
-        /// </summary>
-        public ClientIdScheme ClientIdScheme => AuthorizationRequest.ClientIdScheme;
+    private static IEnumerable<string> GetSanDnsNames(X509Certificate2 certificate)
+    {
+        const string sanOid = "2.5.29.17";
+        var sanNames = new List<string>();
 
-        /// <summary>
-        ///     The client ID of the verifier.
-        /// </summary>
-        public string ClientId => AuthorizationRequest.ClientId;
-
-        private AuthorizationRequest AuthorizationRequest { get; }
-
-        /// <summary>
-        ///     Creates a new instance of the <see cref="RequestObject" /> class.
-        /// </summary>
-        public static implicit operator RequestObject(JwtSecurityToken token) => new(token);
-
-        /// <summary>
-        ///     Creates a new instance of the <see cref="RequestObject" /> class.
-        /// </summary>
-        public static implicit operator JwtSecurityToken(RequestObject requestObject) => requestObject.Value;
-
-        private RequestObject(JwtSecurityToken token)
+        foreach (var extension in certificate.Extensions)
         {
-            AuthorizationRequest = CreateAuthorizationRequest(token.Payload.SerializeToJson());
-            Value = token;
+            if (extension.Oid.Value != sanOid)
+                continue;
+
+            var sanExtension = (AsnEncodedData)extension;
+            var sanData = sanExtension.Format(true);
+
+            foreach (var line in sanData.Split(new[] { "\r\n", "\n", "," }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                sanNames.Add(line.Split(':', '=').Last().Trim());
+            }
         }
 
-        /// <summary>
-        ///     Creates a new instance of the <see cref="RequestObject" /> class.
-        /// </summary>
-        public static RequestObject CreateRequestObject(string requestObjectJson)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-
-            var jwt = tokenHandler.ReadJwtToken(requestObjectJson);
-
-            return new RequestObject(jwt);
-        }
-
-        /// <summary>
-        ///     Gets the authorization request from the request object.
-        /// </summary>
-        public AuthorizationRequest ToAuthorizationRequest() => AuthorizationRequest;
+        return sanNames;
     }
 
     /// <summary>
-    ///     Extension methods for <see cref="RequestObject" />.
+    ///     Validates the trust chain of the leaf certificate
     /// </summary>
-    public static class RequestObjectExtensions
-    {
-        /// <summary>
-        ///     Validates the JWT signature.
-        /// </summary>
-        /// <returns>The validated request object.</returns>
-        /// <exception cref="InvalidOperationException">Throws when validation fails</exception>
-        public static RequestObject ValidateJwt(this RequestObject requestObject) 
-            => ((JwtSecurityToken)requestObject).IsSignatureValid(requestObject.GetLeafCertificate().GetPublicKey()) 
-                ? requestObject 
-                : throw new InvalidOperationException("Invalid JWT Signature");
-        
-        /// <summary>
-        ///     Validates the SAN name of the leaf certificate
-        /// </summary>
-        /// <returns>The validated request object</returns>
-        /// <exception cref="InvalidOperationException">Throws when validation fails</exception>
-        public static RequestObject ValidateSanName(this RequestObject requestObject)
-        {
-            var x509Certificate = new X509Certificate2(
-                requestObject.GetLeafCertificate().GetEncoded()
-            );
-            
-            return GetSanDnsNames(x509Certificate).Any(sanDnsName => sanDnsName == requestObject.ClientId)
-                ? requestObject
-                : throw new InvalidOperationException("SAN does not match Client ID");
-        }
-            
-        private static IEnumerable<string> GetSanDnsNames(X509Certificate2 certificate)
-        {
-            const string sanOid = "2.5.29.17";
-            var sanNames = new List<string>();
+    /// <returns>The validated request object</returns>
+    /// <exception cref="InvalidOperationException">Throws when validation fails</exception>
+    public static RequestObject ValidateTrustChain(this RequestObject requestObject) =>
+        requestObject
+            .GetCertificates()
+            .IsTrustChainValid()
+            ? requestObject
+            : throw new InvalidOperationException("Validation of trust chain failed");
 
-            foreach (var extension in certificate.Extensions)
-            {
-                if (extension.Oid.Value != sanOid) 
-                    continue;
-                
-                var sanExtension = (AsnEncodedData)extension;
-                var sanData = sanExtension.Format(true);
+    internal static List<X509Certificate> GetCertificates(this RequestObject requestObject) =>
+        Parse(((JwtSecurityToken)requestObject).Header.X5c)
+            .Select(
+                certAsJToken =>
+                    new X509CertificateParser()
+                        .ReadCertificate(
+                            FromBase64String(certAsJToken.ToString())
+                        )
+            )
+            .ToList();
 
-                foreach (var line in sanData.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    sanNames.Add(line.Split(':', '=').Last().Trim());
-                }
-            }
-
-            return sanNames;
-        }
-                
-
-        /// <summary>
-        ///     Validates the trust chain of the leaf certificate
-        /// </summary>
-        /// <returns>The validated request object</returns>
-        /// <exception cref="InvalidOperationException">Throws when validation fails</exception>
-        public static RequestObject ValidateTrustChain(this RequestObject requestObject) =>
-            requestObject
-                .GetCertificates()
-                .IsTrustChainValid()
-                ? requestObject
-                : throw new InvalidOperationException("Validation of trust chain failed");
-
-        internal static List<X509Certificate> GetCertificates(this RequestObject requestObject) =>
-            Parse(((JwtSecurityToken)requestObject).Header.X5c)
-                .Select(
-                    certAsJToken =>
-                        new X509CertificateParser()
-                            .ReadCertificate(
-                                FromBase64String(certAsJToken.ToString())
-                            )
-                )
-                .ToList();
-
-        internal static X509Certificate GetLeafCertificate(this RequestObject requestObject) =>
-            GetCertificates(requestObject).First();
-    }
+    internal static X509Certificate GetLeafCertificate(this RequestObject requestObject) =>
+        GetCertificates(requestObject).First();
 }
+
