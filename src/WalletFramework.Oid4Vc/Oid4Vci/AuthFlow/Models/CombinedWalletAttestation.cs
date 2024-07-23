98c0b437d056f9bcc7c2e589e7eb91f355617ac0
Johannes Tuerk <johannes.tuerk@lissi.id>
2024-07-23 13:52:24 +0200

    move structs to own file
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

--- src/WalletFramework.Oid4Vc/Oid4Vci/AuthFlow/Models/CombinedWalletAttestation.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vci/AuthFlow/Models/CombinedWalletAttestation.cs
@@ -1,6 +1,3 @@
-using WalletFramework.Core.Functional;
-using WalletFramework.Core.Functional.Errors;
-
 namespace WalletFramework.Oid4Vc.Oid4Vci.AuthFlow.Models;
 
 public record CombinedWalletAttestation
@@ -28,34 +25,4 @@ public static class CombinedWalletAttestationExtensions
     public static string ToStringRepresentation(this CombinedWalletAttestation combinedWalletAttestation)
         => combinedWalletAttestation.WalletInstanceAttestationJwt + "~" +
            combinedWalletAttestation.WalletInstanceAttestationPopJwt;
-}
-
-public struct WalletInstanceAttestationPopJwt
-{
-    public string Value { get; }
-    
-    public static implicit operator string(WalletInstanceAttestationPopJwt keyId) => keyId.Value;
-    
-    private WalletInstanceAttestationPopJwt(string value) => Value = value;
-
-    public static WalletInstanceAttestationPopJwt CreateWalletInstanceAttestationPopJwt(string value) => new (value);
-}
-
-public struct WalletInstanceAttestationJwt
-{
-    public string Value { get; }
-    
-    public static implicit operator string(WalletInstanceAttestationJwt keyId) => keyId.Value;
-    
-    private WalletInstanceAttestationJwt(string value) => Value = value;
-
-    public static Validation<WalletInstanceAttestationJwt> ValidWalletInstanceAttestationJwt(string value)
-    {
-        if (string.IsNullOrWhiteSpace(value))
-        {
-            return new StringIsNullOrWhitespaceError<WalletInstanceAttestationJwt>();
-        }
-        
-        return new WalletInstanceAttestationJwt(value);
-    }
 }
---
+++ src/WalletFramework.Oid4Vc/Oid4Vci/AuthFlow/Models/WalletInstanceAttestationJwt.cs
@@ -0,0 +1,23 @@
+using WalletFramework.Core.Functional;
+using WalletFramework.Core.Functional.Errors;
+
+namespace WalletFramework.Oid4Vc.Oid4Vci.AuthFlow.Models;
+
+public struct WalletInstanceAttestationJwt
+{
+    public string Value { get; }
+    
+    public static implicit operator string(WalletInstanceAttestationJwt keyId) => keyId.Value;
+    
+    private WalletInstanceAttestationJwt(string value) => Value = value;
+
+    public static Validation<WalletInstanceAttestationJwt> ValidWalletInstanceAttestationJwt(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return new StringIsNullOrWhitespaceError<WalletInstanceAttestationJwt>();
+        }
+        
+        return new WalletInstanceAttestationJwt(value);
+    }
+}
