061b7313ef15e5fe57b4dd19b0cb57b29828c081
Sebastian Bickerle <sebastian.bickerle@lissi.id>
2024-08-21 10:10:34 +0200

    removed custom scheme check on HaipAuthorizationRequestUri.cs
    Signed-off-by: Sebastian Bickerle <sebastian.bickerle@lissi.id>

--- src/WalletFramework.Oid4Vc/Oid4Vp/Models/HaipAuthorizationRequestUri.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vp/Models/HaipAuthorizationRequestUri.cs
@@ -25,9 +25,6 @@ public class HaipAuthorizationRequestUri
     /// <exception cref="InvalidOperationException"></exception>
     public static HaipAuthorizationRequestUri FromUri(Uri uri)
     {
-        if (uri.Scheme is not ("haip" or "openid4vp" or "mdoc-openid4vp"))
-            throw new InvalidOperationException("Invalid Scheme. Must be haip or openid4vp");
-            
         var request = uri.GetQueryParam("request_uri");
         if (string.IsNullOrEmpty(request))
             throw new InvalidOperationException("HAIP requires request_uri parameter");
