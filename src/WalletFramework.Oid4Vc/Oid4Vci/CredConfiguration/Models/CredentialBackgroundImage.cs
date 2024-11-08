3bc18b8936c1cbd965433fa8855b399b405142e6
Johannes Tuerk <johannes.tuerk@lissi.id>
2024-11-08 10:05:03 +0100

    clean backgroundImage model
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

--- src/WalletFramework.Oid4Vc/Oid4Vci/CredConfiguration/Models/CredentialBackgroundImage.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vci/CredConfiguration/Models/CredentialBackgroundImage.cs
@@ -13,71 +13,30 @@ namespace WalletFramework.Oid4Vc.Oid4Vci.CredConfiguration.Models;
 public record CredentialBackgroundImage
 {
     /// <summary>
-    ///     Gets the alternate text that describes the Background Image image. This is typically used for accessibility purposes.
-    /// </summary>
-    public Option<string> AltText { get; }
-
-    /// <summary>
     ///     Gets the URL of the Background Image image.
     /// </summary>
-    public Option<Uri> Uri { get; }
+    public Uri Uri { get; }
     
-    private CredentialBackgroundImage(Option<string> altText, Option<Uri> uri)
+    private CredentialBackgroundImage(Uri uri)
     {
-        AltText = altText;
         Uri = uri;
     }
 
-    public static Option<CredentialBackgroundImage> OptionalCredentialBackgroundImage(JToken logo)
-    {
-        var altText = logo.GetByKey(AltTextJsonKey).ToOption().OnSome(text =>
-        {
-            var str = text.ToString();
-            if (string.IsNullOrWhiteSpace(str))
-                return Option<string>.None;
-
-            return str;
-        });
-        
-        var imageUri = logo.GetByKey(UriJsonKey).ToOption().OnSome(uri =>
-        {
-            try
-            {
-                var str = uri.ToString();
-                var result = new Uri(str);
-                return result;
-            }
-            catch (Exception)
-            {
-                return Option<Uri>.None;
-            }
-        });
-        
-        if (altText.IsNone && imageUri.IsNone)
-            return Option<CredentialBackgroundImage>.None;
-        
-        return new CredentialBackgroundImage(altText, imageUri);
-    }
+    public static Option<CredentialBackgroundImage> OptionalCredentialBackgroundImage(JToken json) 
+        => json.GetByKey(UriJsonKey).ToOption().Match(
+            Some: imageUri => new CredentialBackgroundImage(new Uri(imageUri.ToString())),
+            None: () => Option<CredentialBackgroundImage>.None);
 }
 
 public static class CredentialBackgroundImageJsonExtensions
 {
-    public const string AltTextJsonKey = "alt_text";
     public static string UriJsonKey => "uri";
     
-    public static JObject EncodeToJson(this CredentialBackgroundImage logo)
+    public static JObject EncodeToJson(this CredentialBackgroundImage credentialBackgroundImage)
     {
         var result = new JObject();
 
-        logo.AltText.IfSome(altText =>
-        {
-            result.Add(AltTextJsonKey, altText);
-        });
-
-        logo.Uri.IfSome(uri =>
-        {
-            result.Add(UriJsonKey, uri.ToStringWithoutTrail());
-        });
+        result.Add(UriJsonKey, credentialBackgroundImage.Uri.ToStringWithoutTrail());
 
         return result;
     }
