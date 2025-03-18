7765b3cf06d46281c3a44f009a17057e88575d76
kenkosmowski <ken.kosmowski@gmx.de>
2025-03-18 20:06:09 +0100

    use primary ctor
    Signed-off-by: kenkosmowski <ken.kosmowski@gmx.de>

--- src/WalletFramework.Oid4Vc/Oid4Vp/Services/Oid4VpHaipClient.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vp/Services/Oid4VpHaipClient.cs
@@ -8,30 +8,17 @@ using Format = WalletFramework.Oid4Vc.Oid4Vci.CredConfiguration.Models.Format;
 namespace WalletFramework.Oid4Vc.Oid4Vp.Services;
 
 /// <inheritdoc />
-internal class Oid4VpHaipClient : IOid4VpHaipClient
+internal class Oid4VpHaipClient(
+    IPexService pexService,
+    IDcqlService dcqlService) : IOid4VpHaipClient
 {
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="Oid4VpHaipClient" /> class.
-    /// </summary>
-    /// <param name="pexService">The service responsible for presentation exchange protocol operations.</param>
-    /// <param name="dcqlService">The service responsibl for DCQL operations.</param>
-    public Oid4VpHaipClient(IPexService pexService,
-        IDcqlService dcqlService)
-    {
-        _pexService = pexService;
-        _dcqlService = dcqlService;
-    }
-
-    private readonly IPexService _pexService;
-    private readonly IDcqlService _dcqlService;
-
     /// <inheritdoc />
     public Task<AuthorizationResponse> CreateAuthorizationResponseAsync(
         AuthorizationRequest authorizationRequest,
         (string Identifier, string Presentation, Format Format)[] presentationMap)
     {
         return authorizationRequest.IsPexRequest()
-            ? _pexService.CreateAuthorizationResponseAsync(authorizationRequest, presentationMap)
-            : Task.FromResult(_dcqlService.CreateAuthorizationResponse(authorizationRequest, presentationMap));
+            ? pexService.CreateAuthorizationResponseAsync(authorizationRequest, presentationMap)
+            : Task.FromResult(dcqlService.CreateAuthorizationResponse(authorizationRequest, presentationMap));
     }
 }
