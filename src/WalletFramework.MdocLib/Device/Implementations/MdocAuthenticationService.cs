66054b22be3b7f6b92e4ffe8f14b65c1a2e53d2c
Kevin <kevin.dinh@lissi.id>
2025-03-20 15:27:51 +0100

    fix mdoc device authentication
    Signed-off-by: Kevin <kevin.dinh@lissi.id>

--- src/WalletFramework.MdocLib/Device/Implementations/MdocAuthenticationService.cs
+++ src/WalletFramework.MdocLib/Device/Implementations/MdocAuthenticationService.cs
@@ -6,27 +6,20 @@ using static WalletFramework.MdocLib.Security.Cose.ProtectedHeaders;
 
 namespace WalletFramework.MdocLib.Device.Implementations;
 
-public class MdocAuthenticationService : IMdocAuthenticationService
+public class MdocAuthenticationService(ICoseSign1Signer coseSign1Signer) : IMdocAuthenticationService
 {
-    public MdocAuthenticationService(ICoseSign1Signer coseSign1Signer)
-    {
-        _coseSign1Signer = coseSign1Signer;
-    }
-
-    private readonly ICoseSign1Signer _coseSign1Signer;
-    
     public async Task<AuthenticatedMdoc> Authenticate(Mdoc mdoc, SessionTranscript sessionTranscript, KeyId keyId)
     {
         var deviceNamespaces =
             from keyAuths in mdoc.IssuerSigned.IssuerAuth.Payload.DeviceKeyInfo.KeyAuthorizations
             select keyAuths.ToDeviceNameSpaces();
-
+        
         var deviceAuthentication = new DeviceAuthentication(
             sessionTranscript, mdoc.DocType, deviceNamespaces);
 
         var sigStructure = new SigStructure(deviceAuthentication.ToCbor(), mdoc.IssuerSigned.IssuerAuth.ProtectedHeaders);
 
-        var coseSignature = await _coseSign1Signer.Sign(sigStructure, keyId);
+        var coseSignature = await coseSign1Signer.Sign(sigStructure, keyId);
         
         var deviceSigned = new DeviceSignature(BuildProtectedHeaders(), coseSignature)
             .ToDeviceSigned(deviceNamespaces);
--- src/WalletFramework.Oid4Vc/Oid4Vp/PresentationExchange/Services/PexService.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vp/PresentationExchange/Services/PexService.cs
@@ -156,8 +156,8 @@ public class PexService(
             {
                 return record.Mdoc.IssuerSigned.IssuerAuth.ProtectedHeaders.Value.TryGetValue(new CoseLabel(1), out var alg)
                        && supportedFormatSigningAlgorithms.Match(
-                           formats => formats.MDocFormat?.Alg.Contains(alg.ToString()) ?? true,
-                           () => inputDescriptor.Formats?.MDocFormat?.Alg.Contains(alg.ToString()) ?? true)
+                           formats => formats.MDocFormat?.Alg?.Contains(alg.ToString()) ?? true,
+                           () => inputDescriptor.Formats?.MDocFormat?.Alg?.Contains(alg.ToString()) ?? true)
                        && inputDescriptor.Constraints.Fields!.All(field =>
                        {
                            try
