3aef45fd561e7da71d12a5b7bc96f4038c5d865f
Johannes Tuerk <johannes.tuerk@lissi.id>
2025-06-06 16:49:50 +0200

    Add request_uri_method post support
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

--- src/WalletFramework.Oid4Vc/Oid4Vp/Models/AuthorizationRequestByReference.cs
+++ src/WalletFramework.Oid4Vc/Oid4Vp/Models/AuthorizationRequestByReference.cs
@@ -8,19 +8,23 @@ public record AuthorizationRequestByReference
     public Uri AuthorizationRequestUri { get; }
 
     public Uri RequestUri { get; }
+    
+    public Option<string> RequestUriMethod { get; }
 
-    private AuthorizationRequestByReference(Uri authorizationRequestUri, Uri requestUri) => (AuthorizationRequestUri, RequestUri) = (authorizationRequestUri, requestUri);
+    private AuthorizationRequestByReference(Uri authorizationRequestUri, Uri requestUri, Option<string> requestUriMethod) => 
+        (AuthorizationRequestUri, RequestUri, RequestUriMethod) = (authorizationRequestUri, requestUri, requestUriMethod);
     
     public static Option<AuthorizationRequestByReference> CreateAuthorizationRequestByReference(Uri uri)
     {
         var queryString = HttpUtility.ParseQueryString(uri.Query);
         var clientId = queryString["client_id"];
         var requestUri = queryString["request_uri"];
+        var requestUriMethod = queryString["request_uri_method"];
         
         if (string.IsNullOrEmpty(clientId)
             || string.IsNullOrEmpty(requestUri)) 
             return Option<AuthorizationRequestByReference>.None;
         
-        return new AuthorizationRequestByReference(uri, new Uri(requestUri));
+        return new AuthorizationRequestByReference(uri, new Uri(requestUri), requestUriMethod);
     }
 }
