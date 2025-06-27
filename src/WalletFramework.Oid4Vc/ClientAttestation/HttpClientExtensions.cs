400febf02c5c14971ebbf6c36c502e916896e243
Johannes Tuerk <72355192+JoTiTu@users.noreply.github.com>
2025-06-27 14:48:13 +0200

    Support Client Attestation in VCI Flow (#346)
    * Support Client Attestation in VCI Flow
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * remove lissi specific default value
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    ---------
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

--- src/WalletFramework.Oid4Vc/ClientAttestation/HttpClientExtensions.cs
+++ src/WalletFramework.Oid4Vc/ClientAttestation/HttpClientExtensions.cs
@@ -4,7 +4,10 @@ public static class HttpClientExtensions
 {
     public static void AddClientAttestationPopHeader(this HttpClient client, CombinedWalletAttestation clientAttestation)
     {
+        client.DefaultRequestHeaders.Remove("OAuth-Client-Attestation");
         client.DefaultRequestHeaders.Add("OAuth-Client-Attestation", clientAttestation.WalletInstanceAttestationJwt);
+        
+        client.DefaultRequestHeaders.Remove("OAuth-Client-Attestation-PoP");
         client.DefaultRequestHeaders.Add("OAuth-Client-Attestation-PoP", clientAttestation.WalletInstanceAttestationPopJwt);
     }
 }
