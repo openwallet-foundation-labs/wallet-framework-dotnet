f9f875a84f55097020108105742b17e225eb1f7e
Thomas Röhlich <t.roehlich@esatus.com>
2025-07-17 09:38:23 +0200

    Added SessionEstablishment
    Signed-off-by: Thomas Röhlich <t.roehlich@esatus.com>

---
+++ src/WalletFramework.MdocLib/Security/SessionEstablishment.cs
@@ -0,0 +1,54 @@
+﻿using PeterO.Cbor;
+using WalletFramework.Core.Functional;
+using WalletFramework.MdocLib.Cbor;
+using WalletFramework.MdocLib.Device.Abstractions;
+using WalletFramework.MdocLib.Device.Request;
+using WalletFramework.MdocLib.Security.Cose;
+using WalletFramework.MdocLib.Security.Errors;
+
+namespace WalletFramework.MdocLib.Security;
+
+public record SessionEstablishment(CoseKey EReaderKey, EncryptedDeviceRequest Data);
+
+public static class SessionEstablishmentFun
+{
+    public static CBORObject ToCbor(this SessionEstablishment data)
+    {
+        var result = CBORObject.NewMap();
+
+        var keyLabel = CBORObject.FromObject("eReaderKey");
+        result.Add(keyLabel, data.EReaderKey);
+
+        var dataLabel = CBORObject.FromObject("data");
+        result.Add(dataLabel, data.Data.Encrypted);
+
+        return result;
+    }
+
+    public static Validation<SessionEstablishment> FromCbor(CBORObject sessionEstablishmentCbor, byte[] derivatedKey,
+        IAesGcmEncryption aes)
+    {
+        var data = sessionEstablishmentCbor.GetByLabel("data").OnSuccess(dataCbor =>
+        {
+            var bytes = dataCbor.GetByteString();
+
+            var encryptedRequest = EncryptedDeviceRequest.FromBytes(bytes, derivatedKey, aes);
+            if (encryptedRequest.IsSuccess)
+                return encryptedRequest.Select(request =>
+                    request);
+
+            return new SessionEstablishmentError();
+        });
+        var eReaderKey = sessionEstablishmentCbor.GetByLabel("eReaderKey").OnSuccess(dataCbor =>
+        {
+            var eReaderKey = CoseKey.FromCbor(dataCbor);
+
+            return eReaderKey.IsSuccess ? eReaderKey : new SessionEstablishmentError();
+        });
+
+        return
+            from encrypted in data
+            from key in eReaderKey
+            select new SessionEstablishment(key, encrypted);
+    }
+}
