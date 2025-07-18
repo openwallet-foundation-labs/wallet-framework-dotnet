15f4471cbfdc3ceb7d9169350ae8f824ee3d7220
Johannes Tuerk <72355192+JoTiTu@users.noreply.github.com>
2025-07-18 17:28:05 +0200

    add csc transaction data types and unit tests (#355)
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

--- test/WalletFramework.Oid4Vc.Tests/QCertCreation/Samples/QCertTransactionDataSamples.cs
+++ test/WalletFramework.Oid4Vc.Tests/QCertCreation/Samples/QCertTransactionDataSamples.cs
@@ -7,7 +7,7 @@ namespace WalletFramework.Oid4Vc.Tests.QCertCreation.Samples;
 
 public static class QCertCreationTransactionDataSamples
 {
-    public static string JsonSample => new JObject
+    public static string QCertJsonSample => new JObject
     {
         ["type"] = "qcert_creation_acceptance",
         ["QC_terms_conditions_uri"] = "https://example.com/tos",
@@ -20,10 +20,22 @@ public static class QCertCreationTransactionDataSamples
         },
     }.ToString();
 
-    public static Base64UrlString GetBase64UrlStringSample()
+    public static string CscQCertJsonSample => new JObject
     {
-        var str = JsonSample;
-        var encoded = Base64UrlEncoder.Encode(str);
+        ["type"] = "https://cloudsignatureconsortium.org/2025/qc-request",
+        ["credential_ids"] = new JArray
+        {
+            "credential-id-1"
+        },
+        ["QC_terms_conditions_uri"] = "https://qtsp.example.com/policies/terms_and_conditions.pdf",
+        ["QC_hash"] = "ohxKcClPp/J1dI1iv5x519BpjduGZC794x4ABFeb+Ds=",
+        ["QC_hashAlgorithmOID"] = "2.16.840.1.101.3.4.2.1",
+        ["transaction_data_hashes_alg"] = "sha-256"
+    }.ToString();
+
+    public static Base64UrlString ToBase64UrlString(string qCertJsonString)
+    {
+        var encoded = Base64UrlEncoder.Encode(qCertJsonString);
         return Base64UrlString.FromString(encoded).UnwrapOrThrow();
     }
 }
