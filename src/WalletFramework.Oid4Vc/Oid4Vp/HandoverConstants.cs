bfeaf9b4fb2cc249ccd6e8ee098a6ec1fdeae479
Johannes Tuerk <72355192+JoTiTu@users.noreply.github.com>
2025-09-18 09:07:28 +0200

    Align mdoc response with oid4vp1.0 (#394)
    * Introduce the new mDoc presentation structure
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * Remove Test Dependencies from SdJwtLib
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * change target framework of core project to net8
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * change target framework of mdocLib project to net8
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * clean
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * implemented requested changes
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * xml comment indendation
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * move handover types to constants file
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * revert the Handover union type
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    * fix requested nit
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>
    
    ---------
    
    Signed-off-by: Johannes Tuerk <johannes.tuerk@lissi.id>

---
+++ src/WalletFramework.Oid4Vc/Oid4Vp/HandoverConstants.cs
@@ -0,0 +1,8 @@
+namespace WalletFramework.Oid4Vc.Oid4Vp;
+
+public static class HandoverConstants
+{
+    public const string BasicHandoverTypeIdentifier = "OpenID4VPHandover";
+    
+    public const string DcApiHandoverTypeIdentifier = "OpenID4VPDCAPIHandover";
+}
